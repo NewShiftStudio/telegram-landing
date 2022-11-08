9fbb33b2bb84add65511061ede3b231004c47436
Mikhail Barulin <mikhail-barulin@mail.ru>
2022-11-08 18:36:53 +0300

    feat: 🎸 Обновили с Серегой webpack конфиг для переключения темы

--- next.config.js
+++ next.config.js
@@ -1,7 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
+module.exports = {
+  webpack: config => {
+    const rules = config.module.rules.find(r => !!r.oneOf);
 
-module.exports = nextConfig;
+    rules.oneOf.forEach(loaders => {
+      if (Array.isArray(loaders.use)) {
+        loaders.use.forEach(l => {
+          if (typeof l !== 'string' && typeof l.loader === 'string' && /(?<!post)css-loader/.test(l.loader)) {
+            if (!l.options.modules) return;
+            const { getLocalIdent, ...others } = l.options.modules;
+            l.options = {
+              ...l.options,
+              modules: {
+                ...others,
+                getLocalIdent: (ctx, localIdentName, localName) => {
+                  if (localName === 'dark') return localName;
+                  return getLocalIdent(ctx, localIdentName, localName);
+                },
+              },
+            };
+          }
+        });
+      }
+    });
+
+    return {
+      ...config,
+      reactStrictMode: true,
+      swcMinify: true,
+    };
+  },
+};

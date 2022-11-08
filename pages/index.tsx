e26bc575f372ba5729c97efbc5dd0752e9aff1db
Mikhail Barulin <mikhail-barulin@mail.ru>
2022-11-08 18:39:21 +0300

    feat: 🎸 Обновили тестовую главную страницу

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
 
+import { useDarkMode } from '../hooks/useDarkMode';
 import s from './index.module.scss';
 
 export default function Home() {
+  const { isDark, toggleDark } = useDarkMode();
+
   return (
     <>
       <Head>
@@ -15,6 +17,9 @@ export default function Home() {
       <div className={s.container}>
         <h1 className={s.title}>Hello, World!</h1>
         <p className={s.description}>This next app user tailwind with module scss</p>
+        <button type='button' className={s.button} onClick={toggleDark}>
+          switch to {isDark ? 'light mode' : 'dark mode'}
+        </button>
       </div>
     </>
   );

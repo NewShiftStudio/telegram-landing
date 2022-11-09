7962ed9ce3a33fef3a1fab30f576af667025c76f
Mikhail Barulin <74414622+MikleBr@users.noreply.github.com>
2022-11-09 12:36:12 +0300

    Роутинг (#3)
    * feat: 🎸 Сделал страницу 404
    
    * feat: 🎸 Основа роутинка по чатам
    
    * feat: 🎸 Добавили ssr и рендер 404
    
    Co-authored-by: Mikhail Barulin <mikhail-barulin@mail.ru>

---
+++ constants/chats.ts
@@ -0,0 +1,22 @@
+export const chatsList = [
+  {
+    path: '/',
+    title: 'New Shift',
+    chatContent: 'main chat content',
+  },
+  {
+    path: 'guardBot',
+    title: 'Спортивные Предприниматели',
+    chatContent: '1 Some chat content',
+  },
+  {
+    path: 'questionnaire',
+    title: 'Анкета: Спортивные Предприним...',
+    chatContent: '2 Some chat content',
+  },
+  {
+    path: 'est1993',
+    title: 'est. 1993',
+    chatContent: '3 Some chat content',
+  },
+];

3692a8ab4f41399c1d9c033519cded6dace56f51
Mikhail Barulin <mikhail-barulin@mail.ru>
2022-11-10 15:18:30 +0300

    fix: 🐛 Правки по ревью

--- layouts/AppLayout/AppLayout.tsx
+++ layouts/AppLayout/AppLayout.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router';
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode, useCallback, useState } from 'react';
+
+import cn from 'classnames';
 
 import { Sidebar } from '../../components/Sidebar/Sidebar';
 import { chatsList } from '../../constants/chats';
@@ -15,13 +17,21 @@ export const AppLayout = ({ children }: Props) => {
 
   const [isSidebarOpened, setIsSidebarOpened] = useState(!!query);
 
+  const closeSidebar = useCallback(() => {
+    setIsSidebarOpened(false);
+  }, []);
+
+  const openSidebar = useCallback(() => {
+    setIsSidebarOpened(true);
+  }, []);
+
   return (
-    <div className={`${s.container} ${isSidebarOpened ? s.openedSidebar : ''}`}>
+    <div className={cn(s.container, { [s.openedSidebar]: isSidebarOpened })}>
       <div className={s.sidebar}>
-        <Sidebar chatsList={chatsList} openedLink={query || '/'} onClick={() => setIsSidebarOpened(false)} />
+        <Sidebar chatsList={chatsList} openedLink={query || '/'} onClick={closeSidebar} />
       </div>
       <div className={s.content}>
-        <button type='button' onClick={() => setIsSidebarOpened(true)}>
+        <button type='button' onClick={openSidebar}>
           Назад
         </button>
         {children}

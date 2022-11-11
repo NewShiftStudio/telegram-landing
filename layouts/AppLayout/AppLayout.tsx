702433c21d64bbfeb66656deb177dcd877003d87
Mikhail Barulin <mikhail-barulin@mail.ru>
2022-11-11 12:10:24 +0300

    fix: 🐛 Добавил шапку в AppLayout

--- layouts/AppLayout/AppLayout.tsx
+++ layouts/AppLayout/AppLayout.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode, useCallback, useState } from 'react';
 
 import cn from 'classnames';
 
+import { ChatHeader } from '../../components/Chat/Header/Header';
 import { Sidebar } from '../../components/Sidebar/Sidebar';
 import { chatsList } from '../../constants/chats';
 import s from './AppLayout.module.scss';
@@ -13,9 +14,11 @@ type Props = {
 
 export const AppLayout = ({ children }: Props) => {
   const router = useRouter();
-  const query = router.query.id as string | undefined;
+  const query = (router.query.id || '/') as string;
 
-  const [isSidebarOpened, setIsSidebarOpened] = useState(!!query);
+  const [isSidebarOpened, setIsSidebarOpened] = useState(true);
+
+  const openedChat = chatsList.find(chat => chat.path === query);
 
   const closeSidebar = useCallback(() => {
     setIsSidebarOpened(false);
@@ -28,12 +31,18 @@ export const AppLayout = ({ children }: Props) => {
   return (
     <div className={cn(s.container, { [s.openedSidebar]: isSidebarOpened })}>
       <div className={s.sidebar}>
-        <Sidebar chatsList={chatsList} openedLink={query || '/'} onClick={closeSidebar} />
+        <Sidebar chatsList={chatsList} openedLink={openedChat?.path} onClick={closeSidebar} />
       </div>
       <div className={s.content}>
-        <button type='button' onClick={openSidebar}>
-          Назад
-        </button>
+        <div className={s.header}>
+          <ChatHeader
+            title={openedChat?.title || 'Чат не найден'}
+            image={openedChat?.image?.src}
+            isOnline={!!query}
+            phoneLink='89218650538'
+            onClickBack={openSidebar}
+          />
+        </div>
         {children}
       </div>
     </div>

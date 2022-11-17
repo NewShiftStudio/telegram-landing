576a3300fc49407b83b5e2f8ed78c30e21515ffd
Sergey Smolkov <56842874+H0JLuk@users.noreply.github.com>
2022-11-17 18:19:51 +0300

    feat: 🎸 изменение ширины сайдбара для десктопов (#10)
    * feat: 🎸 возможность смены ширины сайдбара для десктопов
    
    * feat: 🎸 Поправил стили и предельные значения размера сайдбара
    
    * fix: 🐛 Правка по реквесту
    
    Co-authored-by: Mikhail Barulin <mikhail-barulin@mail.ru>

--- layouts/AppLayout/AppLayout.tsx
+++ layouts/AppLayout/AppLayout.tsx
@@ -1,22 +1,42 @@
 import { useRouter } from 'next/router';
-import React, { ReactNode, useCallback, useState } from 'react';
+import { ReactNode, useCallback, useState } from 'react';
 
 import cn from 'classnames';
+import { Resizable, ResizableProps } from 're-resizable';
 
 import { ChatHeader } from '../../components/Chat/Header/Header';
 import { InputBlock } from '../../components/InputBlock/InputBlock';
 import { Sidebar } from '../../components/Sidebar/Sidebar';
 import { chatsList } from '../../constants/chats';
 import { menuLinks, phoneLink } from '../../constants/commonLinks';
+import useMediaQuery from '../../hooks/useMediaQuery';
 import s from './AppLayout.module.scss';
 
 type Props = {
   children: ReactNode;
 };
 
+const resizableProps: ResizableProps = {
+  defaultSize: {
+    width: 350,
+    height: '100%',
+  },
+  minWidth: 330,
+  maxWidth: 500,
+  onResize: (_event, _direction, elemRef, _delta) => {
+    elemRef.style.maxWidth = 'calc(100vw - 350px)';
+  },
+  enable: {
+    right: true,
+    left: true,
+  },
+};
+
 export const AppLayout = ({ children }: Props) => {
   const router = useRouter();
   const query = (router.query.id || '/') as string;
+
+  const isMobile = useMediaQuery('(max-width: 1024px)');
 
   const [isSidebarOpened, setIsSidebarOpened] = useState(true);
 
@@ -36,9 +56,16 @@ export const AppLayout = ({ children }: Props) => {
 
   return (
     <div className={cn(s.container, { [s.openedSidebar]: isSidebarOpened })}>
-      <div className={s.sidebar}>
-        <Sidebar chatsList={chatsList} openedLink={openedChat?.path} onClick={closeSidebar} />
-      </div>
+      {isMobile ? (
+        <div className={s.sidebar}>
+          <Sidebar chatsList={chatsList} openedLink={openedChat?.path} onClick={closeSidebar} />
+        </div>
+      ) : (
+        <Resizable {...resizableProps} className={s.sidebar}>
+          <Sidebar chatsList={chatsList} openedLink={openedChat?.path} onClick={closeSidebar} />
+        </Resizable>
+      )}
+
       <div className={s.content}>
         <div className={s.header}>
           <ChatHeader

68c7b834d7e977f4ec7ba9c4eee8defb18ea6db1
Mikhail Barulin <mikhail-barulin@mail.ru>
2022-11-24 15:10:02 +0300

    fix: 🐛 Поменял все видео на mp4

--- constants/messages/blockchain.ts
+++ constants/messages/blockchain.ts
@@ -4,26 +4,26 @@ export const blockchainMessages = [
   {
     id: 8,
     text: '<b>Криптовалютные приложения</b>\n\nот 50 000 руб.\n\nРазрабатываем мобильные сервисы и адаптируем готовые десктопные версии под iOS и Android. Публикуем приложения в магазинах.',
-    video: '/video/blockchain/mobile.webm',
+    video: '/video/blockchain/mobile.mp4',
   },
   {
     id: 7,
-    video: '/video/blockchain/node.webm',
+    video: '/video/blockchain/node.mp4',
     text: '<b>Установка и настройка ноды</b>\n\nот 20 000 руб.\n\nУстанавливаем и настраиваем узлы blockchain-системы, которые передают информацию о транзакциях и блоках с применением Р2Р-протокола.',
   },
   {
     id: 6,
-    video: '/video/blockchain/api.webm',
+    video: '/video/blockchain/api.mp4',
     text: '<b>Криптовалютные API</b>\n\nот 10 000 руб.\n\nРазрабатываем интерфейсы API, помогающие получать информацию о цифровых валютах и их стоимости. Данные на API поступают с Coinbase, Binance и т. п.',
   },
   {
     id: 5,
-    video: '/video/blockchain/nft.webm',
+    video: '/video/blockchain/nft.mp4',
     text: '<b>NFT-токены</b>\n\nот 5000 руб.\n\nРазрабатываем NFT-токены на базе Ethereum, Binance smart chain, Waves и Stellar. Закрепляем ваши права на уникальный цифровой контент.',
   },
   {
     id: 4,
-    video: '/video/blockchain/smart.webm',
+    video: '/video/blockchain/smart.mp4',
     text: '<b>Smart-контракты</b>\n\nот 3000 руб.\n\nСоздаём smart-контракты на базе платформ Ethereum, Binance smart chain, Waves и Stellar. Разрабатываем алгоритмы для ICO-компаний и на выпуск токенов.',
   },
   {

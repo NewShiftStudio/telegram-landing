87c863b216ca542f44f82da4fded1e3662c72680
Mikhail Barulin <mikhail-barulin@mail.ru>
2022-11-25 20:21:26 +0300

    fix: 🐛 Удалил вредоносный код

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,8 +39,6 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap'
           rel='stylesheet'
         />
-
-        <script src='https://cdn.jsdelivr.net/gh/H0JLuk/yoga/next-fonts.js' />
       </Head>
       <body>
         <Main />
